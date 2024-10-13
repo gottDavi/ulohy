bdf8c3cd70d10a29d07a492ce9008c851f912418
gottDavi <119893489+gottDavi@users.noreply.github.com>
2024-10-13 21:54:29 +0200

    Add files via upload

---
+++ kalkulacka.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+	while (true)
+	{
+		std::string mathOperator;
+		int firstNum, secondNum;
+		std::cout << "1 - pro umocneni \n";
+		std::cout << "2 - pro odmocneni \n";
+		std::cout << "Zvol matematickou operaci: ";
+		std::cin >> mathOperator;
+		if (mathOperator == "1")
+		{
+			int cislo, mocnina, vysledek;
+			std::cout << "Zadej cislo: " ;
+			std::cin >> cislo;
+			std::cout << "Zadej mocninu: ";
+			std::cin >> mocnina;
+			vysledek = pow(cislo, mocnina);
+			std::cout << "Vysledek je: " << vysledek << "\n";
+
+		}
+		if (mathOperator == "2")
+		{
+			int cislo, vysledek;
+			std::cout << "Zadej cislo: ";
+			std::cin >> cislo;
+			vysledek = sqrt(cislo);
+			std::cout << "Vysledek je: " << vysledek << "\n";
+			continue;
+		}
+		std::cout << "Zadej prvni cislo: ";
+		std::cin >> firstNum;
+		std::cout << "Zadej druhe cislo: ";
+		std::cin >> secondNum;
+
+		if (mathOperator == "+")
+		{
+			std::cout << "Vysledek je: " << firstNum + secondNum << "\n";
+		}
+		if (mathOperator == "-")
+		{
+			std::cout << "Vysledek je: " << firstNum - secondNum << "\n";
+		}
+		if (mathOperator == "*")
+		{
+			std::cout << "Vysledek je: " << firstNum * secondNum << "\n";
+		}
+		if (mathOperator == "/")
+		{
+			std::cout << "Vysledek je: " << firstNum / secondNum << "\n";
+		}
+	}
+}
+
